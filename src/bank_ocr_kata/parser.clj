8fc7425cd503983338d11fb258df5660676e0b34
Adam Lukens <alukens@purduefed.com>
2016-03-18 04:15:59 -0400

    Add error-handling for single digits
    If we don't find the specified digit, return an X so we know it's unparseable.

--- src/bank_ocr_kata/parser.clj
+++ src/bank_ocr_kata/parser.clj
@@ -57,9 +57,9 @@
              " _|"))
 
 (def digits
-  { zero 0, one 1, two 2, three 3, four 4, five 5, six 6, seven 7, eight 8, nine 9 })
+  { zero "0", one "1", two "2", three "3", four "4", five "5", six "6", seven "7", eight "8", nine "9" })
 
 (defn parse-single-number [input-string]
   "Takes a 4 line by 3 character string and parses to a number"
-  (digits input-string))
+  (get digits input-string "X"))
 
--- test/bank_ocr_kata/parser_test.clj
+++ test/bank_ocr_kata/parser_test.clj
@@ -2,17 +2,23 @@
   (:require [clojure.test :refer :all]
             [bank-ocr-kata.parser :refer :all]))
 
+(def invalid-digit
+   (one-line "/o/"
+             " / "
+             "/ /"))
+
 (deftest test-parse-single-number
   (testing "parses a single account digit"
     (are [x y] (= x (parse-single-number y))
-      0 zero
-      1 one
-      2 two
-      3 three
-      4 four
-      5 five
-      6 six
-      7 seven
-      8 eight
-      9 nine)))
+      "0" zero
+      "1" one
+      "2" two
+      "3" three
+      "4" four
+      "5" five
+      "6" six
+      "7" seven
+      "8" eight
+      "9" nine
+      "X" invalid-digit)))
 
