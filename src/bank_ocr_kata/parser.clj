f86c9d21b23833645ed555b66adfc3c940ae2ac1
Adam Lukens <alukens@purduefed.com>
2016-03-18 04:04:41 -0400

    Cover the remaining single digits

--- src/bank_ocr_kata/parser.clj
+++ src/bank_ocr_kata/parser.clj
@@ -6,13 +6,58 @@
    the source code"
   (clojure.string/join "\n" lines))
 
+(def zero
+   (one-line " _ "
+             "| |"
+             "|_|"))
+
 (def one
    (one-line "   "
              "  |"
              "  |"))
 
+(def two
+   (one-line " _ "
+             " _|"
+             "|_ "))
+
+(def three
+   (one-line " _ "
+             " _|"
+             " _|"))
+
+(def four
+   (one-line "   "
+             "|_|"
+             "  |"))
+
+(def five
+   (one-line " _ "
+             "|_ "
+             " _|"))
+
+(def six
+   (one-line " _ "
+             "|_ "
+             "|_|"))
+
+(def seven
+   (one-line " _ "
+             "  |"
+             "  |"))
+
+(def eight
+   (one-line " _ "
+             "|_|"
+             "|_|"))
+
+(def nine
+   (one-line " _ "
+             "|_|"
+             " _|"))
+
 (def digits
-  { one 1 })
+  { zero 0, one 1, two 2, three 3, four 4, five 5, six 6, seven 7, eight 8, nine 9 })
 
 (defn parse-single-number [input-string]
   "Takes a 4 line by 3 character string and parses to a number"
--- test/bank_ocr_kata/parser_test.clj
+++ test/bank_ocr_kata/parser_test.clj
@@ -2,12 +2,17 @@
   (:require [clojure.test :refer :all]
             [bank-ocr-kata.parser :refer :all]))
 
-(def one
-   (one-line "   "
-             "  |"
-             "  |"))
-
 (deftest test-parse-single-number
-  (testing "parses 1"
-    (is (= 1 (parse-single-number one)))))
+  (testing "parses a single account digit"
+    (are [x y] (= x (parse-single-number y))
+      0 zero
+      1 one
+      2 two
+      3 three
+      4 four
+      5 five
+      6 six
+      7 seven
+      8 eight
+      9 nine)))
 
