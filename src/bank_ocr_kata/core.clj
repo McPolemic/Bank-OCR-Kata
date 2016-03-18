7c5df024c7623b84b6591c4c1b3ee0937a773a46
Adam Lukens <alukens@purduefed.com>
2016-03-18 02:58:58 -0400

    Initial import

---
+++ src/bank_ocr_kata/core.clj
@@ -0,0 +1,7 @@
+(ns bank-ocr-kata.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/bank_ocr_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns bank-ocr-kata.core-test
+  (:require [clojure.test :refer :all]
+            [bank-ocr-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
